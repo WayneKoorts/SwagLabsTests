414686bd4a513195a0f12e67c7028c26aec5976e
Wayne Koorts <wayne@wkoorts.com>
2026-03-08 16:54:40 +1300

    Add test users static class.

---
+++ src/SwagLabsTests/TestUsers.cs
@@ -0,0 +1,14 @@
+namespace SwagLabsTests;
+
+public static class TestUsers
+{
+    // Password is publically displayed on the login screen.
+    public const string Password = "secret_sauce";
+
+    public const string StandardUser = "standard_user";
+    public const string LockedOutUser = "locked_out_user";
+    public const string ProblemUser = "problem_user";
+    public const string PerformanceGlitchUser = "performance_glitch_user";
+    public const string ErrorUser = "error_user";
+    public const string VisualUser = "visual_user";
+}
